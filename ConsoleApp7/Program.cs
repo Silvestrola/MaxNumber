771423f45195d872714d0fab2f14a8a9276d7ed9
guilherme.araujo <guilherme.araujo@BR-J5N0B6000127>
2023-08-18 11:24:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            int n = 0, r = 0, y;
+            List<int> sequencia = new List<int>();
+
+            for (y = 0; y < 3; y++)
+            {
+                r++;
+                Console.Write("Informe o " + (r) + "° número: ");
+                while (!int.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.Write("Número invalido: ");
+                }
+                sequencia.Add(n);
+            }
+            Console.WriteLine("Número maximo: " + sequencia.Max());           
+        }
+    }
+}
